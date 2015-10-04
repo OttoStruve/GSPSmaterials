11573141b9ea93093c949d50b272d10fdb0c2650
kgullikson88 <kevin.gullikson@gmail.com>
2015-10-04 09:59:21 -0500

    Updated reveal.js url.

--- Kevin-Mar2015/open_slideshow.sh
+++ Kevin-Mar2015/open_slideshow.sh
@@ -1,2 +1,3 @@
 #/bin/bash
 ipython nbconvert Plotting\ in\ Python.ipynb --to slides --post serve --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.6.2"
+#ipython nbconvert Plotting\ in\ Python.ipynb --to slides --post serve --reveal-prefix "https://github.com/hakimel/reveal.js/"
